890996d4e6d87ef0b7ed4a477cfa3e539ba4c340
David Griffiths <dogriffiths@gmail.com>
2019-11-19 10:32:07 +0000

    refactor: Remove unused imports

--- relish-selenide/src/main/java/uk/co/blackpepper/relish/selenide/RadioButtons.java
+++ relish-selenide/src/main/java/uk/co/blackpepper/relish/selenide/RadioButtons.java
@@ -3,22 +3,11 @@ package uk.co.blackpepper.relish.selenide;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.Select;
-
 import uk.co.blackpepper.relish.core.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.getSelectedRadio;
-import static com.codeborne.selenide.Selenide.selectRadio;
-import static org.hamcrest.Matchers.contains;
-import static org.junit.Assert.assertThat;
 import static uk.co.blackpepper.relish.core.TestUtils.attempt;
 
 /**
